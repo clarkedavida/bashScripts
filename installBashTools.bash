6603f19dd0139f3135517eebd73984f2a2140be1
david clarke <clarke.davida@gmail.com>
2022-10-14 12:10:22 -0600

    fixed bug in installBashTools

--- installBashTools.bash
+++ installBashTools.bash
@@ -6,8 +6,10 @@ bashrcFile=${HOME}/.bashrc
 echo "" >> ${bashrcFile}
 echo "# bashTools" >> ${bashrcFile}
 echo "export bashToolsPath='${currentDirectory}'" >> ${bashrcFile}
-echo "alias whatsInQueue='${bashToolsPath}/slurm/whatsInQueue.bash'" >> ${bashrcFile}
-echo "alias nodeList='${bashToolsPath}/slurm/nodeList.bash'" >> ${bashrcFile}
+echo "alias whatsInQueue=\"\${bashToolsPath}/slurm/whatsInQueue.bash\"" >> ${bashrcFile}
+echo "alias nodeList=\"\${bashToolsPath}/slurm/nodeList.bash\"" >> ${bashrcFile}
+
+
 
 source ${bashrcFile}
 
